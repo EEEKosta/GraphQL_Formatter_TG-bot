b27a9e6981bb546bc95b3abf81707b01452cea47
Константин Егоров <ekosta@MacBook-Air-Konstantin.local>
2023-12-02 16:17:35 +0300

    Minor updates

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
+import logging
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 import graphql
 
+# Установите ваш токен Telegram бота
+TOKEN = 'YOUR_TOKEN'
 
-def format_graphql_query(query: str) -> str:
+# Включаем логирование для получения информации о возможных ошибках
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+def start(update: Update) -> None:
+    update.message.reply_text(
+        'Привет! Я бот GraphQL Formatter. Просто отправь мне GraphQL-запрос, и я форматирую его для тебя.')
+
+
+def format_graphql(update: Update) -> None:
+    try:
+        # Получаем текст сообщения с GraphQL-запросом от пользователя
+        user_query = update.message.text
+        # Парсим и форматируем GraphQL-запрос
+        formatted_query = graphql_format(user_query)
+        # Отправляем отформатированный запрос обратно пользователю
+        update.message.reply_text(f'Вот отформатированный запрос:\n{formatted_query}')
+    except Exception as e:
+        # В случае ошибки отправляем пользователю сообщение об ошибке
+        update.message.reply_text(f'Произошла ошибка: {str(e)}')
+
+
+def graphql_format(query: str) -> str:
     try:
         # Парсим запрос с использованием библиотеки graphql-core-next
         parsed_query = graphql.parse(query)
-        # Форматируем запрос и возвращаем отформатированный текст
+        # Форматируем запрос
         formatted_query = graphql.print_ast(parsed_query)
         return formatted_query
     except graphql.GraphQLError as e:
@@ -13,8 +40,25 @@ def format_graphql_query(query: str) -> str:
         raise Exception(f'Ошибка форматирования GraphQL-запроса: {str(e)}')
 
 
-# Ввод GraphQL-запроса из консоли
-user_query = input("Введите ваш GraphQL-запрос:\n")
+def main() -> None:
+    # Инициализируем бота с использованием токена
+    updater = Updater(TOKEN)
 
-formatted_query = format_graphql_query(user_query)
-print(f'\nОтформатированный запрос:\n{formatted_query}')
+    # Получаем диспетчер бота
+    dp = updater.dispatcher
+
+    # Добавляем обработчик команды /start
+    dp.add_handler(CommandHandler("start", start))
+
+    # Добавляем обработчик для текстовых сообщений, содержащих GraphQL-запросы
+    dp.add_handler(MessageHandler(filters.text & ~filters.command, format_graphql))
+
+    # Запускаем бота
+    updater.start_polling()
+
+    # Оставляем бота в режиме работы до принудительного завершения
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
